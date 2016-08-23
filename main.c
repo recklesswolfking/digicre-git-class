075188fcd2ec21167d1841b8e19627ae9cbf13cc
recklesswolfking <recklesswolfking@gmail.com>
2016-08-23 10:00:07 +0900

    initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Github1
+//
+//  Created by 許哲 on 2016/08/23.
+//  Copyright © 2016年 許哲. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void){
+    for(int i = 0;i < 10;i++){
+        printf("%d\n",i*i);
+    }
+    return 0;
+}
